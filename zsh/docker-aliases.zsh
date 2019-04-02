e91de420ace0823d2404c7db064755ceb3243b1d
Richard de Wit <henk.exe@gmail.com>
2019-04-02 14:19:26 +0200

    Add docker stop alias

--- zsh/docker-aliases.zsh
+++ zsh/docker-aliases.zsh
@@ -8,5 +8,6 @@ alias dce='docker-compose exec'
 alias dcr='docker-compose down && docker-compose up --build'
 alias dcrd='docker-compose down && docker-compose up -d --build'
 
+alias dstop='docker stop $(docker ps -a -q)'
 alias dtop='docker stats --format "table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}  {{.MemPerc}}\t{{.NetIO}}\t{{.BlockIO}}"'
 alias dnuke='docker system prune --volumes'
