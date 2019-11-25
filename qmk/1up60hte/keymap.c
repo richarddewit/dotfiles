ce60b6e9e0609731a8528d283aa24ccb13dcb764
Richard de Wit <henk.exe@gmail.com>
2019-11-25 14:27:05 +0100

    Add actual Dvorak

--- qmk/1up60hte/keymap.c
+++ qmk/1up60hte/keymap.c
@@ -47,10 +47,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,    KC_LGUI,   KC_LALT,                                    FN_SPC,                                                KC_RALT,   KC_RGUI,   KC_RCTL),
 
   [_DVORAK] = LAYOUT_tsangan(
-      KC_ESC,     KC_1,      KC_2,       KC_3,     KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSLS,   FN_GRV,
-      KC_TAB,     KC_Q,      KC_W,       KC_E,     KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSPC,
-      CTL_ESC,    KC_A,      KC_S,       KC_D,     KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,              KC_ENT ,
-      KC_LSFT,    KC_Z,      KC_X,       KC_C,     KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,              KC_DEL ,
+      KC_ESC,     KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_LBRC,   KC_RBRC,   KC_BSLS,   FN_GRV,
+      KC_TAB,     KC_QUOTE,  KC_COMM,   KC_DOT,    KC_P,      KC_Y,      KC_F,      KC_G,      KC_C,      KC_R,      KC_L,      KC_SLSH,   KC_EQL,    KC_BSPC,
+      CTL_ESC,    KC_A,      KC_O,      KC_E,      KC_U,      KC_I,      KC_D,      KC_H,      KC_T,      KC_N,      KC_S,      KC_MINS,              KC_ENT ,
+      KC_LSFT,    KC_SCLN,   KC_Q,      KC_J,      KC_K,      KC_X,      KC_B,      KC_M,      KC_W,      KC_V,      KC_Z,      KC_RSFT,              KC_DEL ,
       KC_LCTL,    KC_LGUI,   KC_LALT,                                    FN_SPC,                                                KC_RALT,   KC_RGUI,   KC_RCTL),
 
   [_GAME] = LAYOUT_tsangan(
