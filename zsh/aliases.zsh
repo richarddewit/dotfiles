7417a35d73d4829f02381bda46d08bc39c72199a
Richard de Wit <henk.exe@gmail.com>
2020-01-23 08:51:23 +0100

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
-function run_bg {
+run_bg() {
   nohup "$@" &>/dev/null & disown %%
 }
 
@@ -8,68 +8,63 @@ alias sr="exec $SHELL"
 
 alias vim=nvim
 
-function mkcd {
-  command mkdir -p $1 && cd $1
+mkcd() {
+  command mkdir -p "$1" && cd "$1"
 }
-alias mkcd=mkcd
 alias open=xdg-open
 [ -f "$HOME/go/bin/g" ] && unalias g
 
 # Node
-alias npmg='npm i -g'
-alias npms='npm i -S'
-alias npmd='npm i -D'
-alias nvmrc='nvm current > .nvmrc'
+alias npmg="npm i -g"
+alias npms="npm i -S"
+alias npmd="npm i -D"
+alias nvmrc="nvm current > .nvmrc"
 
 # Vagrant
-alias vsus='vagrant suspend'
-alias vssh='vagrant ssh'
-alias vup='vagrant up'
+alias vsus="vagrant suspend"
+alias vssh="vagrant ssh"
+alias vup="vagrant up"
 
 # Editors/IDEs
-alias gmacs='run_bg emacs $@'
-alias pstorm='run_bg phpstorm $@'
+function gmacs {
+  if [ -z "$@" ]; then
+    args="."
+  else
+    args="$@"
+  fi
+  run_bg emacs "$args"
+}
 
 # Listing with directory first
-alias l='ls -lah --group-directories-first'
-alias la='ls -lAh --group-directories-first'
-alias ll='ls -alh --color=auto --group-directories-first'
-alias ls='ls -lh --color=auto --group-directories-first'
-alias lsa='ls -lah --group-directories-first'
-
-# Homestead
-function homestead() {
-  ( cd ~/Homestead && vagrant $* )
-}
-alias hs=homestead
-alias hssus="homestead suspend"
-alias hsssh="homestead ssh -- -t \"cd projects/`basename \"$PWD\"`; /bin/bash\""
-alias hsus=hssus
-alias hssh=hsssh
-alias hsedit="vim ~/Homestead/Homestead.yaml"
+alias l="ls -lah --group-directories-first"
+alias la="ls -lAh --group-directories-first"
+alias ll="ls -alh --color=auto --group-directories-first"
+alias ls="ls -lh --color=auto --group-directories-first"
+alias lsa="ls -lah --group-directories-first"
 
 # Python/Django
-alias m='python manage.py'
-alias pi='pipenv install'
+alias py="python"
+alias m="python manage.py"
+alias pi="pipenv install"
 
 # Emacs
-function emc() {
-  dir=${1:-.}
+emc() {
+  dir="${1:-.}"
   if [ -n "$(ps -ax | grep "[e]macs --daemon")" ]; then
-    emacsclient -a '' -n $dir
+    emacsclient -a "" -n "$dir"
   else
-    emacsclient -a '' -c -n $dir
+    emacsclient -a "" -c -n "$dir"
   fi
 }
 
 # Docker
 source $DOTFILES/zsh/docker-aliases.zsh
 
-function mine() {
+mine() {
   if [ -z "$@" ]; then
-    args='.'
+    args="."
   else
     args="$@"
   fi
-  sudo chown -R $(id -nu):$(id -ng) $args
+  sudo chown -R $(id -nu):$(id -ng) "$args"
 }
